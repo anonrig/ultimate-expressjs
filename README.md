ce2f2f7c11c59abd14d23ebd1fa0523e965449ac
Yagiz Nizipli <yagiznizipli@me.com>
2019-12-20 19:48:45 +0300

    :sparkles: Remove coralogix and update boom

--- README.md
+++ README.md
@@ -3,60 +3,48 @@
 Tired of adding express, method-override, cors, compression, method-override etc. to all of your expressjs frameworks? Here is the solution:
 
 ```javascript
-const http = require('ultimate-expressjs');
-const express = new http({port: 3000});
+const http = require("ultimate-expressjs");
+const express = new http({ port: 3000 });
 
-express.setRoutes = (app) => {
-  app.get('/', () => req.json({hello: 'world'}));
+express.setRoutes = app => {
+  app.get("/", () => req.json({ hello: "world" }));
 };
 
-express.listen().then(() => console.info(`Server started`))
-
+express.listen().then(() => console.info(`Server started`));
 ```
 
 Pretty cool isn't it?
-
 
 ### Overriding setRoutes
 
 In order to set your own routes, use the exaple code
 
-
 ```javascript
-express.setRoutes = (app) => {
+express.setRoutes = app => {
   // access your expressjs instance through app parameter.
-}
+};
 ```
-
 
 ### Overriding setErrorHandlers
 
 In order to have your own commands for error handling (if you don't want to use default Boom):
 
 ```javascript
-express.setErrorHandlers = (app) => {
+express.setErrorHandlers = app => {
   // access your expressjs instance through app parameter
-}
+};
 ```
 
-### Coralogix integration
+### Custom logging Support
 
-In order to use Coralogix as your default logger for logging errors, please use the following code:
+In order to use custom loggers as your default logger, please use the following code:
 
 ```javascript
-const expressLogger = (error) => {
-  const log = new Coralogix.Log({
-    severity:Coralogix.Severity.error,
-    className: 'express.js',
-    methodName: 'express',
-    text: error
-  })
-  errorLogger.addLog(log)
-}
-const express = new http({port: PORT, logger: expressLogger})
+const customLogger = error => {
+  // send data to anywhere you want.
+};
+const express = new http({ port: PORT, logger: customLogger });
 ```
-
-UltimateExpress package automatically checks if `coralogix-logger` is installed in your project and if succeed, converts your logger into a callback function to send your unique log to Coralogix. If you want to remove this default behavior just set `http.isCoralogixInstalled = true`
 
 ### Rate Limit Support
 
@@ -70,24 +58,26 @@ const express = new http({
   logger: expressLogger,
   limiterOptions: {
     store: new RedisStore({
-      client: require('redis').createClient(process.env.REDIS_URL)
+      client: require("redis").createClient(process.env.REDIS_URL)
     }),
     max: 10,
     delay: 0,
     windowMs: 1 * 60 * 1000
   },
   handler: (req, res, next) => {
-    suspiciousLogger.addLog(new Coralogix.Log({
-      severity: Coralogix.Severity.warning,
-      className: 'index.js',
-      methodName: 'rateLimiter',
-      text: {
-        message: `${req.headers['cf-connecting-ip']} is sending too many requests`,
-        query: req.query
-      }
-    }))
+    suspiciousLogger.addLog(
+      new Coralogix.Log({
+        severity: Coralogix.Severity.warning,
+        className: "index.js",
+        methodName: "rateLimiter",
+        text: {
+          message: `${req.headers["cf-connecting-ip"]} is sending too many requests`,
+          query: req.query
+        }
+      })
+    );
 
-    next(Boom.tooManyRequests('You have exceeded your rate limit.'))
+    next(Boom.tooManyRequests("You have exceeded your rate limit."));
   }
-})
+});
 ```
--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const compression = require('compression');
-const methodOverride = require('method-override');
-const cors = require('cors');
-const Boom = require('boom');
-const {isCoralogixInstalled} = require('./helper')
-const RateLimiter = require('express-rate-limit')
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const compression = require("compression");
+const methodOverride = require("method-override");
+const cors = require("cors");
+const Boom = require("@hapi/boom");
+const RateLimiter = require("express-rate-limit");
+const isFunction = require("lodash.isfunction");
 
 class Server {
-  constructor({port, logger, limiterOptions} = {}) {
+  constructor({ port, logger, limiterOptions } = {}) {
     this.app = express();
     this.server = http.Server(this.app);
     this.port = port;
     this.logger = logger || console;
-    this.isCoralogixInstalled = isCoralogixInstalled;
-    this.limiterEnabled = !!limiterOptions
-    this.limiterOptions = limiterOptions
+    this.limiterEnabled = !!limiterOptions;
+    this.limiterOptions = limiterOptions;
 
-    this.app.enable('trust proxy');
+    this.app.enable("trust proxy");
     this.app.use(cors());
     this.app.use(helmet());
     this.app.use(compression());
-    this.app.use(methodOverride('X-HTTP-Method'));
-    this.app.use(methodOverride('X-HTTP-Method-Override'));
-    this.app.use(methodOverride('X-Method-Override'));
+    this.app.use(methodOverride("X-HTTP-Method"));
+    this.app.use(methodOverride("X-HTTP-Method-Override"));
+    this.app.use(methodOverride("X-Method-Override"));
     this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({
-      extended: true
-    }));
+    this.app.use(
+      bodyParser.urlencoded({
+        extended: true
+      })
+    );
 
     if (this.limiterEnabled) {
-      this.limiter = new RateLimiter(this.limiterOptions)
-      this.app.use(this.limiter)
+      this.limiter = new RateLimiter(this.limiterOptions);
+      this.app.use(this.limiter);
     }
   }
 
@@ -41,50 +42,49 @@ class Server {
     /**
      * Just override this function to set your routes.
      */
-   }
+  }
 
   setErrorHandlers() {
     /**
      * 404 pages.
      */
     this.app.use((req, res) => {
-      const message = Boom.notFound('Not found');
+      const message = Boom.notFound("Not found");
       res.status(404).json(message.output.payload);
     });
 
     /* Handle errors */
     this.app.use((err, req, res, next) => {
       if (err) {
-        if (this.isCoralogixInstalled) {
+        if (isFunction(this.logger)) {
           this.logger(err);
         } else {
           this.logger.error(err);
         }
-        const {
-          statusCode,
-          payload
-        } = err.isBoom ? err.output :
-            err.isJoi ? Boom.badRequest(err.details[0].message).output :
-            Boom.boomify(err).output;
+        const { statusCode, payload } = err.isBoom
+          ? err.output
+          : err.isJoi
+          ? Boom.badRequest(err.details[0].message).output
+          : Boom.boomify(err).output;
 
         return res
           .status(statusCode)
-          .json(Object.assign({}, payload, {payload: err.data}));
+          .json(Object.assign({}, payload, { payload: err.data }));
       }
 
-      const {
-        statusCode, payload
-      } = Boom.badImplementation('Server error').output;
+      const { statusCode, payload } = Boom.badImplementation(
+        "Server error"
+      ).output;
       res.status(statusCode).json(payload);
     });
-   }
+  }
 
-   listen() {
+  listen() {
     this.setRoutes(this.app);
-    this.setErrorHandlers(this.app)
+    this.setErrorHandlers(this.app);
 
     return new Promise((resolve, reject) => {
-      this.server.listen(this.port, (error) => {
+      this.server.listen(this.port, error => {
         if (error) return reject(error);
 
         resolve();
